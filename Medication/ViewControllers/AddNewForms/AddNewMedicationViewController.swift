f495e30499ffbe5deb6938d62f76bb56857d0b14
DevarshiK <devarshi.bluechip@gmail.com>
2016-09-05 19:12:36 +0530

    Corrected Anys to Any

--- Medication/ViewControllers/AddNewForms/AddNewMedicationViewController.swift
+++ Medication/ViewControllers/AddNewForms/AddNewMedicationViewController.swift
@@ -54,7 +54,7 @@ class AddNewMedicationViewController: FormBaseViewController {
         configurePickerViews()
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Anys?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if let segueIdentifier = segue.identifier {
             if segueIdentifier == MedicinesTableViewController.pushSegueIdentifier {
